e6fbfbffda5930536219daec1c0efb12514c06b6
Peter Rasmussen <peter@soundboks.com>
2021-10-15 13:15:19 +0200

    first working example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Debug, StructOpt)]
+#[structopt(name = "radix", about = "Tool that will convert input number of any radix to dec, hex and bin.")]
+struct Opt {
+    input: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut radix:u32 = 10;
+    let input;
+    let opt = Opt::from_args();
+    match opt.input {
+        Some(v) => input = v,
+        None => panic!("Not a valid input"),
+    };
+    let stripped;
+    match input.strip_prefix("0x") {
+        Some(v) => {
+            stripped = v;
+            radix = 16;
+        }
+        None => stripped = &input,
+    }
+
+    match i64::from_str_radix(stripped, radix) {
+        Ok(v) => {
+            println!("dec: {:}", v);
+            println!("hex: {:#x}", v);
+            println!("bin: {:#b}", v);
+        },
+        Err(e) => panic!("{}", e),
+    }
+
+
 }
