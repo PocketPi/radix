cce6c31f22d7ef809915896ec07c302d3134654b
Peter Rasmussen <peter@soundboks.com>
2021-10-18 15:15:28 +0200

    add width support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+use std::fmt;
 use std::num::ParseIntError;
 
 use structopt::StructOpt;
@@ -11,13 +13,40 @@ use structopt::StructOpt;
 struct Opt {
     input: String,
 
-    #[structopt(short, long, default_value="32")]
-    width: i64,
+    #[structopt(short, long, default_value = "32")]
+    width: usize,
 }
 
-fn main() -> Result<(), ParseIntError> {
+#[derive(Debug)]
+enum RadixError {
+    InvalidWidth,
+    InvalidInput(ParseIntError),
+}
+
+impl fmt::Display for RadixError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            RadixError::InvalidWidth => f.write_str("Invalid width"),
+            RadixError::InvalidInput(i) => fmt::Display::fmt(&i, f),
+        }
+    }
+}
+
+impl Error for RadixError {}
+
+fn main() {
+    _main().unwrap_or_else(|err| {
+        println!("{}", err);
+    })
+}
+
+fn _main() -> Result<(), RadixError> {
     let mut radix: u32 = 10;
     let opt = Opt::from_args();
+
+    if (opt.width % 2) != 0 {
+        return Err(RadixError::InvalidWidth);
+    }
 
     let input = opt.input;
     let stripped = match input.strip_prefix("0x") {
@@ -28,18 +57,20 @@ fn main() -> Result<(), ParseIntError> {
         None => &input,
     };
 
-    let v = i64::from_str_radix(stripped, radix).or_else(|e| {
-        if radix == 10 {
-            println!("Failed parsing in radix 10. Trying radix 16!");
-            i64::from_str_radix(stripped, 16)
-        } else {
-            Err(e)
-        }
-    })?;
+    let v = i64::from_str_radix(stripped, radix)
+        .or_else(|e| {
+            if radix == 10 {
+                println!("Failed parsing in radix 10. Trying radix 16!");
+                i64::from_str_radix(stripped, 16)
+            } else {
+                Err(e)
+            }
+        })
+        .map_err(|e| RadixError::InvalidInput(e))?;
 
     println!("dec: {:}", v);
-    println!("hex: 0x{:x}", v);
-    println!("bin: {:#018b}", v);
+    println!("hex: 0x{:0>width$x}", v, width = opt.width / 8);
+    println!("bin: 0b{:0>width$b}", v, width = opt.width / 2);
 
     Ok(())
 }
