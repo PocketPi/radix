7ff4aa36f87992c1d82b65101774ba988d596093
Peter Rasmussen <peter@soundboks.com>
2021-10-18 09:27:55 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,45 @@
+use std::num::ParseIntError;
+
 use structopt::StructOpt;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Debug, StructOpt)]
-#[structopt(name = "radix", about = "Tool that will convert input number of any radix to dec, hex and bin.")]
+#[structopt(
+    name = "radix",
+    about = "Tool that will convert input number of any radix to dec, hex and bin."
+)]
 struct Opt {
-    input: Option<String>,
+    input: String,
+
+    #[structopt(short, long, default_value="32")]
+    width: i64,
 }
 
-fn main() {
-    let mut radix:u32 = 10;
-    let input;
+fn main() -> Result<(), ParseIntError> {
+    let mut radix: u32 = 10;
     let opt = Opt::from_args();
-    match opt.input {
-        Some(v) => input = v,
-        None => panic!("Not a valid input"),
-    };
-    let stripped;
-    match input.strip_prefix("0x") {
+
+    let input = opt.input;
+    let stripped = match input.strip_prefix("0x") {
         Some(v) => {
-            stripped = v;
             radix = 16;
+            v
         }
-        None => stripped = &input,
-    }
+        None => &input,
+    };
 
-    match i64::from_str_radix(stripped, radix) {
-        Ok(v) => {
-            println!("dec: {:}", v);
-            println!("hex: {:#x}", v);
-            println!("bin: {:#b}", v);
-        },
-        Err(e) => panic!("{}", e),
-    }
+    let v = i64::from_str_radix(stripped, radix).or_else(|e| {
+        if radix == 10 {
+            println!("Failed parsing in radix 10. Trying radix 16!");
+            i64::from_str_radix(stripped, 16)
+        } else {
+            Err(e)
+        }
+    })?;
 
+    println!("dec: {:}", v);
+    println!("hex: 0x{:x}", v);
+    println!("bin: {:#018b}", v);
 
+    Ok(())
 }
