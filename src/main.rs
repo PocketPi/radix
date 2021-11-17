67ab1de1f5e214ab4375a5ba3f9634427e0b4abb
Peter Rasmussen <peter@soundboks.com>
2021-11-17 23:13:44 +0100

    add width option

--- src/main.rs
+++ src/main.rs
@@ -52,39 +52,73 @@ fn parse_hex(src: &str) -> Result<isize, RadixError> {
 #[derive(Debug, StructOpt)]
 #[structopt(
     name = "radix",
-    about = "Tool that will convert input number of any radix to dec, hex and bin."
+    about = "Tool that will convert input number of any radix to dec, hex and bin.",
+    author = "Peter Rasmussen",
+    global_settings = &[structopt::clap::AppSettings::ArgRequiredElseHelp,
+                        structopt::clap::AppSettings::AllowNegativeNumbers]
 )]
 struct Opt {
-    #[structopt(parse(try_from_str = parse_hex))]
-    input1: isize,
 
+    /// Required argument, input value in dec or hex format
+    #[structopt(parse(try_from_str = parse_hex))]
+    input1: Option<isize>,
+
+    /// Required if two input values are given
+    #[structopt(requires("input2"), possible_values(&["+", "-", "*", "/", "%"]))]
     operator: Option<String>,
 
-    #[structopt(parse(try_from_str = parse_hex))]
+    /// If <operator> and <input2> values are given simple math will be happen
+    #[structopt(parse(try_from_str = parse_hex), requires("operator"))]
     input2: Option<isize>,
+
+    /// Width determines how many bits is used to show the result value.
+    #[structopt(short, long, requires("input1"), possible_values(&["2", "4", "8", "16", "32", "64"]))]
+    width: Option<usize>,
 }
 
 fn main() -> Result<(), RadixError> {
     let opt = Opt::from_args();
 
-    let mut result = opt.input1;
-
-    if let Some(o) = opt.operator {
-        if let Some(v) = opt.input2 {
-            result = match o.as_str() {
-                "+" => result.wrapping_add(v),
-                "-" => result.wrapping_sub(v),
-                "*" => result.wrapping_mul(v),
-                "/" => result.wrapping_div(v),
-                "%" => result % v,
-                _ => return Err(RadixError::NotImplementedOperator),
+    if let Some(mut r) = opt.input1 {
+        if let Some(o) = opt.operator {
+            if let Some(v) = opt.input2 {
+                r = match o.as_str() {
+                    "+" => r.wrapping_add(v),
+                    "-" => r.wrapping_sub(v),
+                    "*" => r.wrapping_mul(v),
+                    "/" => r.wrapping_div(v),
+                    "%" => r % v,
+                    _ => return Err(RadixError::NotImplementedOperator),
+                };
             };
         };
-    };
 
-    println!("dec: {:}", result);
-    println!("hex: 0x{:x}", result);
-    println!("bin: 0b{:b}", result);
+        let mut width: usize = 2;
+        if r / 2_isize.pow(4) >= 1 {
+            width = 8;
+            if r / 2_isize.pow(width as u32) >= 1 {
+                width = 16;
+                if (r / 2_isize.pow(width as u32)) >= 1 {
+                    width = 32;
+                    if (r / 2_isize.pow(width as u32)) >= 1 {
+                        width = 64;
+                    }
+                }
+            }
+        }
+
+        if let Some(w) = opt.width {
+            if width > w {
+                println!("Width not wide enough to reprecent result, setting width to: {:}", width);
+            } else {
+                width = w;
+            }
+        }
+
+        println!("dec: {:}", r);
+        println!("hex: 0x{:01$x}", r, width / 4);
+        println!("bin: 0b{:01$b}", r, width);
+    };
 
     Ok(())
 }
